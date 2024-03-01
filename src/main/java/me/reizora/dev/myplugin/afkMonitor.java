31b975d70ff26b43e39b1a8df5d1182869598dca
Reizora <lauglaugjefter@gmail.com>
2024-03-01 14:00:52 +0800

    removed import of playerteleportevent in afkmonitor.java

--- src/main/java/me/reizora/dev/myplugin/afkMonitor.java
+++ src/main/java/me/reizora/dev/myplugin/afkMonitor.java
@@ -7,7 +7,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
