f3881e56008b0ba352c071a02b16c8b766fbae58
mortymort123 <sortigosac@gmail.com>
2024-03-14 22:49:39 +0800

    AFK Monitoring and Lava Grass

--- src/main/java/me/reizora/dev/myplugin/MyPlugin.java
+++ src/main/java/me/reizora/dev/myplugin/MyPlugin.java
@@ -1,48 +1,19 @@
 package me.reizora.dev.myplugin;
 
-import me.reizora.dev.myplugin.commands.eventCommands;
-import me.reizora.dev.myplugin.commands.playerCommands;
-import me.reizora.dev.myplugin.commands.serverCommands;
-import me.reizora.dev.myplugin.listeners.entityEvents;
-import me.reizora.dev.myplugin.listeners.objectEvents;
-import me.reizora.dev.myplugin.listeners.playerEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 public class MyPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        System.out.println("PLUGIN HAS STARTED!!!!!!!!!!!!!");
-        objectEvents obj = new objectEvents();
-        playerEvents player = new playerEvents();
-        entityEvents entity = new entityEvents();
-        afkMonitor afk = new afkMonitor(this);
-        afk.startIdleCheckTask();
-
-        //eventAPIs
-        getServer().getPluginManager().registerEvents(obj, this);
-        getServer().getPluginManager().registerEvents(player, this);
-        getServer().getPluginManager().registerEvents(entity, this);
-
-        //eventCommands
-        getCommand("break").setExecutor(new eventCommands(obj));
-        getCommand("place").setExecutor(new eventCommands(obj));
-        //playerCommands
-        getCommand("fly").setExecutor(new playerCommands());
-        getCommand("craft").setExecutor(new playerCommands());
-        getCommand("creative").setExecutor(new playerCommands());
-        getCommand("survival").setExecutor(new playerCommands());
-        getCommand("night").setExecutor(new playerCommands());
-        getCommand("day").setExecutor(new playerCommands());
-        getCommand("setspawn").setExecutor(new playerCommands());
-        //serverCommands
-        getCommand("setMOTD").setExecutor(new serverCommands());
-        getCommand("setSize").setExecutor(new serverCommands());
-        getCommand("setTimeout").setExecutor(new serverCommands());
-        getCommand("setAFKTimer").setExecutor(new serverCommands());
-        getCommand("getAFKTimer").setExecutor(new serverCommands());
+        PlayerListener playerListener = new PlayerListener();
+        getServer().getPluginManager().registerEvents(playerListener   ,this);
+        System.out.println("The plugin has started");
+        Bukkit.getScheduler().runTaskTimer(this, PlayerMonitor::checkAfk,0,1);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getScheduler().cancelTasks(this);
     }
 }
